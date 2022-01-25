7c6ea390fe1fa62c205776249afc70a726df0b52
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-01-25 21:53:14 +0900

    fix bug

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ sudo apt-get install -y tmux ;
 sudo apt-get install -y vim ;
 sudo apt-get install -y zsh ;
 
+# install cargo
+curl https://sh.rustup.rs -sSf | sh
+
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ;
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ;
@@ -16,6 +19,18 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 # install neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage ;
+mv nvim.appimage ~/  ;
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' ;
+
+# install nodejs
+cd ~
+curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh ;
+sudo bash nodesource_setup.sh
+sudo apt-get install -y nodejs ;
+
+npm install -g yarn ;
+
 
 # copy config files
 cp ~/configuration/.zshrc ~/ ;
