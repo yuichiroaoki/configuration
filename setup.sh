4b611b27db6fd0c41a37690d0e4f5e77a69bebc9
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-01-25 21:07:12 +0900

    neovim plugin

--- setup.sh
+++ setup.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+# install packages
+sudo apt-get update ;
+sudo apt-get install -y tmux ;
+sudo apt-get install -y vim ;
+sudo apt-get install -y zsh ;
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ;
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ;
+git clone https://github.com/jeffreytse/zsh-vi-mode \
+  $ZSH/custom/plugins/zsh-vi-mode ;
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ;
+
+# install neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage ;
+
+# copy config files
 cp ~/configuration/.zshrc ~/ ;
 cp ~/configuration/.bashrc ~/ ;
 
