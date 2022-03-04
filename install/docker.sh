cc1ea26f4ac062b311326de2e8f1c4c299057c78
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-03-04 14:34:46 +0900

    docker-compose

--- install/docker.sh
+++ install/docker.sh
@@ -24,7 +24,3 @@ sudo usermod -aG docker $USER ;
 
 newgrp docker ;
 
-# install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose ;
-
-sudo chmod +x /usr/local/bin/docker-compose ;
