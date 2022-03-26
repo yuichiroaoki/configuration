4440271a0a7463311b0421a3af294508bf56c4a1
yuichiroaoki <yuichiroaoki27@gmail.com>
2022-03-26 20:22:43 +0900

    update

--- gce-ubuntu.sh
+++ gce-ubuntu.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # install packages
-sudo apt-get update ;
-sudo apt-get install -y tmux ;
-sudo apt-get install -y vim ;
-sudo apt-get install -y zsh ;
-sudo apt-get install -y xclip ;
+sudo apt-get update
+sudo apt-get install -y tmux
+sudo apt-get install -y zsh
+sudo apt-get install -y xclip
 
 # install cargo
 curl https://sh.rustup.rs -sSf | sh
@@ -13,33 +12,32 @@ curl https://sh.rustup.rs -sSf | sh
 
 # vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim ;
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # oh my zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ;
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ;
-git clone https://github.com/jeffreytse/zsh-vi-mode \
-  $ZSH/custom/plugins/zsh-vi-mode ;
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ;
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # install neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage ;
-mv nvim.appimage ~/  ;
+chmod u+x nvim.appimage
+mv nvim.appimage ~/
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' ;
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 
 # copy config files
-cp ~/configuration/.zshrc ~/ ;
-cp ~/configuration/.bashrc ~/ ;
+cp ~/configuration/.zshrc ~/
+cp ~/configuration/.bashrc ~/
 
 
-sudo cp  ~/configuration/etc/vim/vimrc.*  /etc/vim/  ;
-mkdir -p  ~/.config/nvim && cp ~/configuration/init.vim ~/.config/nvim/init.vim ;
-cp ~/configuration/.tmux.conf ~/.tmux.conf  ;
-cp ~/configuration/.vimrc ~/.vimrc  ;
+sudo cp  ~/configuration/etc/vim/vimrc.*  /etc/vim/
+mkdir -p  ~/.config/nvim && cp ~/configuration/init.vim ~/.config/nvim/init.vim
+cp ~/configuration/.tmux.conf ~/.tmux.conf
+cp ~/configuration/.vimrc ~/.vimrc
 
