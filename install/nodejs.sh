f29214f0537629269a2c405afcd8f671c04572ad
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-03-30 14:40:49 +0900

    update nodejs install

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -2,9 +2,8 @@
 
 # install nodejs
 cd ~
-curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh ;
-sudo bash nodesource_setup.sh
-sudo apt-get install -y nodejs ;
 
-sudo npm install -g yarn ;
+# Using Ubuntu
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
