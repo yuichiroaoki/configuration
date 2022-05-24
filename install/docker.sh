1ce96b321a834f958580dcc3aba4835e44d78b9b
yuichiroaoki <yuichiroaoki27@gmail.com>
2022-05-24 15:30:17 +0900

    exec

--- install/docker.sh
+++ install/docker.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # install Docker for Ubuntu
-sudo apt-get update ;
+sudo apt-get update -y
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
- curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg ;
+    lsb-release -y
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null ;
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update ;
- sudo apt-get install docker-ce docker-ce-cli containerd.io -y ;
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 
 # For compute engine
-sudo groupadd docker ;
+sudo groupadd docker
 
-sudo usermod -aG docker $USER ;
+sudo usermod -aG docker $USER
 
-newgrp docker ;
+newgrp docker
 
